086bd7ee4336e2d2b557c6879bc197dee7ea7519
ajneung <45349191+ajneung@users.noreply.github.com>
2019-02-21 14:18:33 +0700

    Add files via upload

---
+++ esp8266-wifi.ino
@@ -0,0 +1,53 @@
+/*
+ *  This sketch sends data via HTTP GET requests to data.sparkfun.com service.
+ *
+ *  You need to get streamId and privateKey at data.sparkfun.com and paste them
+ *  below. Or just customize this script to talk to other HTTP servers.
+ *
+ */
+
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "SMART-FARM";
+const char* password = "12345678";
+
+void connectWIFI() {
+  // We start by connecting to a WiFi network
+
+  //Serial.println();
+  //Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  //WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+void setup(){
+  Serial.begin(9600);
+  //delay(10);
+  connectWIFI();
+}
+int value = 0;
+
+void loop() {
+  delay(2000);
+  ++value;
+  Serial.println("connected");
+  Serial.println(WiFi.localIP());
+}
+
